59d9327ace28e9bf2ef9309becfd3a5aa69963ca
Florian BRUNIAUX <florian@bruniaux.com>
2026-02-10 17:26:20 +0100

    ccboard: remove redundant version line
    Homebrew extracts version from URL tag automatically

--- Formula/ccboard.rb
+++ Formula/ccboard.rb
@@ -3,7 +3,6 @@ class Ccboard < Formula
   homepage "https://github.com/FlorianBruniaux/ccboard"
   url "https://github.com/FlorianBruniaux/ccboard/archive/refs/tags/v0.5.2.tar.gz"
   sha256 "7515bf959b73b956ceb967351c7e299cbb3668a53d35f9c770eb72e00d93ced6"
-  version "0.5.2"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
